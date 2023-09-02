1d7c023b3836f5f97547beb00ba9bbba6f8b2c45
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-09-02 23:26:00 +0200

    make pretty_print function work with restaurant

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod parser;
 mod structs;
 use crate::structs::*;
 use colored::Colorize;
+use inflector::cases::titlecase::to_title_case;
 use std::fs;
 use url::Url;
 
@@ -11,18 +12,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let base_restaurant_url = Url::parse(&fs::read_to_string("./restaurant.txt")?)?;
     let document = fetch::fetch_doc(base_restaurant_url.as_str()).await?;
 
-    let restaurants = parser::get_restaurants(&document, base_restaurant_url);
-    let menus = parser::get_menus(&document)?;
+    let restaurants = parser::get_restaurants(&document, base_restaurant_url).await?;
 
-    println!("{:#?}", restaurants.await?);
+    // println!("{:#?}", restaurants.await?);
+    pretty_print_restaurants(restaurants);
     Ok(())
 }
 
-fn pretty_print_menu(menus: Vec<Menu>) {
-    for menu in menus {
-        println!("{}", menu.date.format("%d.%m").to_string().red());
-        for meal in menu.meals {
-            println!("{}", meal.name);
+fn pretty_print_restaurants(restaurants: Vec<Restaurant>) {
+    for restaurant in restaurants {
+        println!();
+        println!(
+            "{}",
+            to_title_case(restaurant.name.to_lowercase().as_str())
+                .blue()
+                .bold()
+        );
+        for menu in restaurant.menus {
+            println!("{}", menu.date.format("%d.%m").to_string().red());
+            for meal in menu.meals {
+                println!("{}", meal.name);
+            }
         }
     }
 }
