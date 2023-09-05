5e0f2cd2a447bbb0988e0912f76e8e032bb6c466
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-09-05 14:20:09 +0200

    print out error message

--- src/structs/sv_error.rs
+++ src/structs/sv_error.rs
@@ -9,6 +9,6 @@ impl std::error::Error for SvError {}
 
 impl Display for SvError {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "No menus found in provided sv-restaurant URL")
+        write!(f, "{}", self.message)
     }
 }
