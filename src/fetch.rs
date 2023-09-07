8b6e74c6220bc1d1e540ff8ed61aa98f9bd5737e
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-09-07 18:05:53 +0200

    create restaurant search post request

--- src/fetch.rs
+++ src/fetch.rs
@@ -5,3 +5,16 @@ pub async fn fetch_doc(url: &str) -> Result<Html, reqwest::Error> {
     let res = client.get(url).send().await?.text().await?;
     Ok(Html::parse_document(&res))
 }
+
+pub async fn fetch_restaurants(searchfield: &str) -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let params = [("searchfield", searchfield)];
+    let result = client
+        .post("https://www.sv-restaurant.ch/de/mensen/mensa-suche?type=8700")
+        .form(&params)
+        .send()
+        .await?
+        .text()
+        .await?;
+    Ok(result)
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ use url::Url;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // print_restaurants().await
+    println!("{:#?}", fetch::fetch_restaurants("bkw").await?);
+
+    Ok(())
+}
+
+async fn print_restaurants() -> Result<(), Box<dyn std::error::Error>> {
     let base_restaurant_url = Url::parse(&fs::read_to_string("./restaurant.txt")?)?;
     let document = fetch::fetch_doc(base_restaurant_url.as_str()).await?;
 
